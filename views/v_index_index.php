78601e88dc816e88b70d3ed7f5a6274cff2b55b6
Shirley Hui <shuiallen@gmail.com>
2013-10-30 11:18:40 -0400

    Fix validation errors

--- views/v_index_index.php
+++ views/v_index_index.php
@@ -3,26 +3,27 @@
 <p>
 	Have fun discussing Nanners!
 </p>
-<p>
-	<h1> Status </h1>
-		<ul> Landing page - this page updated with current status </ul>
-		<ul> Navigation bar with different options if user is logged in or not - done</ul>
-		<ul> Sign up - done</ul>
-		<ul> Log in  - done</ul>
-		<ul> Log out  - done</ul>
-		<ul> See a list of all other users - done</ul>
-		<ul> Follow or unfollow users </ul>
-		<ul> View followed posts - does not include self </ul>
-		<ul> +1 feature - View my posts and all posts - done </ul>
-		<ul> +1 feature - Display logged in user's profile, it's not beautiful</ul>
-		<ul> +1 feature - Upload an image and display on profile page </ul>
-		<ul> Basic look and feel - only the outside container and nav bar</ul>
 
-		<ul> todo Live DB created, but test DB not transferred yet </ul>
-		<ul> todo Review for DRY </ul>
-		<ul> todo check error handling </ul>
-		<ul> todo code validation </ul>
-		<ul> Defect: login with bad input or no input displays page without formatting</ul>
-		<ul> Defect: Placement of replace profile photo is not correct </ul>
-		<ul> Enhance: Redirect after posting should do something better </ul>
-</p>
+<h1> Status </h1>
+	<ul>
+		<li> Landing page - this page updated with current status </li>
+		<li> Navigation bar with different options if user is logged in or not - done</li>
+		<li> Sign up - done</li>
+		<li> Log in  - done</li>
+		<li> Log out  - done</li>
+		<li> See a list of all other users - done</li>
+		<li> Follow or unfollow users </li>
+		<li> View followed posts - does not include self </li>
+		<li> +1 feature - View my posts and all posts - done </li>
+		<li> +1 feature - Display logged in user's profile, it's not beautiful</li>
+		<li> +1 feature - Upload an image and display on profile page </li>
+		<li> Basic look and feel - only the outside container and nav bar</li>
+
+		<li> todo Live DB created, but test DB not transferred yet </li>
+		<li> todo Review for DRY </li>
+		<li> todo check error handling </li>
+		<li> todo code validation </li>
+		<li> Defect: login with bad input or no input displays page without formatting</li>
+		<li> Defect: Placement of replace profile photo is not correct </li>
+		<li> Enhance: Redirect after posting should do something better </li>
+	 </ul>
