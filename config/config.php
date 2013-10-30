c8549c18477c18fda1bf459140d35cac31735f26
Shirley Hui <shuiallen@gmail.com>
2013-10-30 11:59:42 -0400

    fix email address

--- config/config.php
+++ config/config.php
@@ -28,7 +28,7 @@ A email designated to receive messages from the server. Examples:
  	
  	Some might want this to be the same as the APP_EMAIL, others might want to create a designated gmail address for it
 */ 	
-	define('SYSTEM_EMAIL', 'webmaster@myapp.com'); 
+	define('SYSTEM_EMAIL', 'shuiallen@yahoo.com'); 
 
 # Default DB name for this app
 	define('DB_NAME', "nannersb_p2_nanners_biz"); 
